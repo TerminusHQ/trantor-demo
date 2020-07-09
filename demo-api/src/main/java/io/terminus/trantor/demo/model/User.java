999349587400f18cf066c62ecc091c15cfeed8fc
Jiang Zhengdong <zhengdong.jzd@alibaba-inc.com>
2020-07-09 15:59:03 +0800

    feature: init trantor demo

---
+++ demo-api/src/main/java/io/terminus/trantor/demo/model/User.java
@@ -0,0 +1,24 @@
+package io.terminus.trantor.demo.model;
+
+import io.terminus.trantor.api.annotation.BaseModel;
+import io.terminus.trantor.api.annotation.TModel;
+import io.terminus.trantor.api.annotation.TModelField;
+import io.terminus.trantor.api.annotation.TModelFieldType;
+
+/**
+ * @author zhengdong.jzd@alibaba-inc.com
+ * @date 2020/7/8
+ */
+@TModel(name = "用户信息")
+public class User extends BaseModel<Long> {
+    private static final long serialVersionUID = 1L;
+
+    @TModelField(name = "用户名", nullable = false)
+    private String name;
+
+    @TModelField(name = "邮箱")
+    private String email;
+
+    @TModelField(name = "电话")
+    private String phone;
+}
