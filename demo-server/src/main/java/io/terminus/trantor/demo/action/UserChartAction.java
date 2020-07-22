4ddc63b1558413201775343e2266eb0c3319378a
18521097617 <hsc170165@alibaba-inc.com>
2020-07-22 13:43:06 +0800

    chart demo服务

--- demo-server/src/main/java/io/terminus/trantor/demo/action/UserChartAction.java
+++ demo-server/src/main/java/io/terminus/trantor/demo/action/UserChartAction.java
@@ -1,5 +1,6 @@
 package io.terminus.trantor.demo.action;
 
+import io.terminus.trantor.module.base.model.DashboardChartItem;
 import io.terminus.trantor.sdk.datasource.ChartDataAction;
 import io.terminus.trantor.sdk.datasource.ChartDataParams;
 import io.terminus.trantor.sdk.datasource.ChartDataResult;
@@ -18,26 +19,26 @@ public class UserChartAction implements ChartDataAction {
 
     public ChartDataResult load(ChartDataParams chartDataParams) {
         ChartDataResult chartDataResult = new ChartDataResult();
-        List<ChartDataResult.ChartDataItem> result = new ArrayList<ChartDataResult.ChartDataItem>();
-        ChartDataResult.ChartDataItem item = new ChartDataResult.ChartDataItem();
+        List<DashboardChartItem> result = new ArrayList<DashboardChartItem>();
+        DashboardChartItem item = new DashboardChartItem();
         item.setGroup("2020-07-16");
         item.setLabel("水果");
         item.setValue(432l);
         result.add(item);
 
-        ChartDataResult.ChartDataItem item1 = new ChartDataResult.ChartDataItem();
+        DashboardChartItem item1 = new DashboardChartItem();
         item1.setGroup("2020-07-15");
         item1.setLabel("药品");
         item1.setValue(2312l);
         result.add(item1);
 
-        ChartDataResult.ChartDataItem item2 = new ChartDataResult.ChartDataItem();
+        DashboardChartItem item2 = new DashboardChartItem();
         item2.setGroup("2020-07-14");
         item2.setLabel("肉禽");
         item2.setValue(332l);
         result.add(item2);
 
-        ChartDataResult.ChartDataItem item3 = new ChartDataResult.ChartDataItem();
+        DashboardChartItem item3 = new DashboardChartItem();
         item3.setGroup("2020-07-13");
         item3.setLabel("蔬菜");
         item3.setValue(154l);
