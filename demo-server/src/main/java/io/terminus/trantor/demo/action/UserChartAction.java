b785760e137aa644f7731c364ab66735594cd826
ronggeng <gengrong.gr@alibaba-inc.com>
2020-11-10 14:03:30 +0800

    update 0.13

--- demo-server/src/main/java/io/terminus/trantor/demo/action/UserChartAction.java
+++ demo-server/src/main/java/io/terminus/trantor/demo/action/UserChartAction.java
@@ -1,6 +1,7 @@
 package io.terminus.trantor.demo.action;
 
 import com.alibaba.fastjson.JSON;
+import io.terminus.trantor.module.base.model.DashboardChartItem;
 import io.terminus.trantor.sdk.datasource.ChartDataAction;
 import io.terminus.trantor.sdk.datasource.ChartDataParams;
 import io.terminus.trantor.sdk.datasource.ChartDataResult;
@@ -21,26 +22,26 @@ public class UserChartAction implements ChartDataAction {
     public ChartDataResult load(ChartDataParams chartDataParams) {
         log.info("chartDataParams info: " + JSON.toJSONString(chartDataParams));
         ChartDataResult chartDataResult = new ChartDataResult();
-        List<ChartDataResult.DashboardChartItem> result = new ArrayList<ChartDataResult.DashboardChartItem>();
-        ChartDataResult.DashboardChartItem item = new ChartDataResult.DashboardChartItem();
+        List<DashboardChartItem> result = new ArrayList<DashboardChartItem>();
+        DashboardChartItem item = new DashboardChartItem();
         item.setGroup("2020-07-16");
         item.setLabel("水果");
         item.setValue(432l);
         result.add(item);
 
-        ChartDataResult.DashboardChartItem item1 = new ChartDataResult.DashboardChartItem();
+        DashboardChartItem item1 = new DashboardChartItem();
         item1.setGroup("2020-07-15");
         item1.setLabel("药品");
         item1.setValue(2312l);
         result.add(item1);
 
-        ChartDataResult.DashboardChartItem item2 = new ChartDataResult.DashboardChartItem();
+        DashboardChartItem item2 = new DashboardChartItem();
         item2.setGroup("2020-07-14");
         item2.setLabel("肉禽");
         item2.setValue(332l);
         result.add(item2);
 
-        ChartDataResult.DashboardChartItem item3 = new ChartDataResult.DashboardChartItem();
+        DashboardChartItem item3 = new DashboardChartItem();
         item3.setGroup("2020-07-13");
         item3.setLabel("蔬菜");
         item3.setValue(154l);
