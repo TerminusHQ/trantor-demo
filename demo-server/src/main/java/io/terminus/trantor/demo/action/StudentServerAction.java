e41d95cf40d34e13ac05d24ba3e28301c94dd13d
yaomingfeng <wb-ymf644622@alibaba-inc.com>
2021-01-22 11:18:00 +0800

    add log

---
+++ demo-server/src/main/java/io/terminus/trantor/demo/action/StudentServerAction.java
@@ -0,0 +1,15 @@
+package io.terminus.trantor.demo.action;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 用户模型自定义 server action
+ *
+ * @author zhengdong.jzd@alibaba-inc.com
+ * @date 2020/7/13
+ */
+@Component
+public class StudentServerAction {
+
+
+}
