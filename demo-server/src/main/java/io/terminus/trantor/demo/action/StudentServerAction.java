d0a24adb71ed6d7891ad31aec0ebe59069079118
yaomingfeng <wb-ymf644622@alibaba-inc.com>
2021-02-02 16:55:25 +0800

    serveraction

--- demo-server/src/main/java/io/terminus/trantor/demo/action/StudentServerAction.java
+++ demo-server/src/main/java/io/terminus/trantor/demo/action/StudentServerAction.java
@@ -1,13 +1,19 @@
 package io.terminus.trantor.demo.action;
 
 import io.terminus.trantor.api.TContext;
+import io.terminus.trantor.api.annotation.RootModel;
 import io.terminus.trantor.api.annotation.TAction;
+import io.terminus.trantor.api.annotation.params.TCollectionParam;
 import io.terminus.trantor.api.annotation.params.TParam;
 import io.terminus.trantor.demo.dao.StudentRepository;
 import io.terminus.trantor.demo.model.Classes;
 import io.terminus.trantor.demo.model.Student;
+import io.terminus.trantor.sdk.autumn.param.Operators;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 学生相关 server action
@@ -34,5 +40,38 @@ public class StudentServerAction {
     }
 
 
+    @TAction(modelClass = Student.class)
+    public void batchsave(@TCollectionParam(Student.class) List<Student> students) {
 
+        studentRepo.saveWithRelationSkipNull(students);
+    }
+
+
+    @TAction(modelClass = Student.class)
+    public void update(@TParam Student student) {
+
+        studentRepo.updateConnectRelationSkipNull(student);
+    }
+
+    @TAction(modelClass = Student.class)
+    public void batchupdate(@TCollectionParam(Student.class) List<Student> students) {
+
+        studentRepo.updateConnectRelationSkipNull(students);
+    }
+
+    @TAction(modelClass = Student.class)
+    public void batchdelete(@TCollectionParam(Student.class) List<Student> students) {
+
+        List<Long> ids = students.stream().map(RootModel::getId).collect(Collectors.toList());
+
+        studentRepo.delete(ids);
+    }
+
+    @TAction(modelClass = Student.class)
+    public void deleteByCondition(String name, String id) {
+        studentRepo.delete(student -> {
+            student.condition(Student.name_field, Operators.CONTAINS, name);
+            student.condition(Student.id_field, Operators.EQ, id);
+        });
+    }
 }
