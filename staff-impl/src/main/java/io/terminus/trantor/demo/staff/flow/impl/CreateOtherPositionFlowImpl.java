42dd347cc301c67b6d34a6a52b6492168da71e93
刘亚月 <liuyayue@liuyayuedeMacBook-Pro.local>
2021-04-22 23:10:22 +0800

    fix:

--- staff-impl/src/main/java/io/terminus/trantor/demo/staff/flow/impl/CreateOtherPositionFlowImpl.java
+++ staff-impl/src/main/java/io/terminus/trantor/demo/staff/flow/impl/CreateOtherPositionFlowImpl.java
@@ -3,11 +3,15 @@ package io.terminus.trantor.demo.staff.flow.impl;
 import io.terminus.trantor.demo.staff.flow.CreatePositionFlow;
 import io.terminus.trantor.demo.staff.func.CreatePositionFunc;
 import io.terminus.trantor.demo.staff.model.Position;
+import io.terminus.trantorframework.api.annotation.FlowImpl;
+import io.terminus.trantorframework.api.annotation.RouteRuleValue;
 
 /**
  * @author liuyayue
  * @create 2021/4/22 08:53 下午
  */
+@FlowImpl(name = "create other position with dynamic route")
+@RouteRuleValue({"middle","primary"})
 public class CreateOtherPositionFlowImpl implements CreatePositionFlow {
 
     private CreatePositionFunc createPositionFunc;
