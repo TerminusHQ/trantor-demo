c7b6ca63e2e0f1bb88b23a534732d05e72e166da
chengong <cg268505@alibaba-inc.com>
2021-05-06 18:34:11 +0800

    增加standard-module 模块，便于增加二开模块

--- standard-module/demo-runtime/src/main/java/io/terminus/trantor/demo/Application.java
+++ standard-module/demo-runtime/src/main/java/io/terminus/trantor/demo/Application.java
@@ -4,6 +4,10 @@ package io.terminus.trantor.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author liuyayue
+ * @create 2021/4/25 10:59 上午
+ */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
